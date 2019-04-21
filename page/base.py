b76808aa20deeaa6d3d6f1c0351fae5ac526bba0
heber <1@1.com>
2019-04-21 18:17:16 +0800

    haha

---
+++ page/base.py
@@ -0,0 +1,9 @@
+import urllib.request,time
+
+c_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+try:
+    urllib.request.urlopen("http://www.leyou999.com")
+
+    print("%s 网站正常" %c_time)
+except Exception as e:
+    print(e)
