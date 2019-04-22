d84879a845c2da98417519094d5a186b2f3a8509
xinpeter <1@1.com>
2019-04-22 10:07:42 +0800

    test1

--- page/base.py
+++ page/base.py
@@ -2,7 +2,7 @@ import urllib.request,time
 
 c_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 try:
-    urllib.request.urlopen("http://www.leyou999.com")
+    urllib.request.urlopen("http://www.leyou9990.com")
 
     print("%s 网站正常3" %c_time)
 except Exception as e:
