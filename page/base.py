49591865f657cd1eda9fed967e83c48c731e444c
heber <123123123@qq.com>
2019-05-19 10:56:30 +0800

    uuu

--- page/base.py
+++ page/base.py
@@ -4,7 +4,7 @@ class Test_url:
     def test_url(self):
         c_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         try:
-            data = urllib.request.urlopen("http://www.leyou999.com")
+            data = urloolib.request.urlopen("http://www.leyou999.com")
             print("%s 网站正常3" % c_time)
             assert True
         except Exception as e:
